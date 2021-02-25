ceed669f979f64b08c3fb490f3501777414e9a45
yazdan27 <yazdan.t2002@gmail.com>
2021-02-25 19:24:39 +0100

    login en sign up pages

--- lib/Screens/Login/components/body.dart
+++ lib/Screens/Login/components/body.dart
@@ -1,48 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:healthai/Screens/Login/login_screen.dart';
+import 'package:healthai/Screens/Login/components/background.dart';
 import 'package:healthai/Screens/Signup/signup_screen.dart';
-import 'package:healthai/Screens/Welcome/components/background.dart';
+import 'package:healthai/components/already_have_an_account_acheck.dart';
 import 'package:healthai/components/rounded_button.dart';
-import 'package:healthai/constants.dart';
+import 'package:healthai/components/rounded_input_field.dart';
+import 'package:healthai/components/rounded_password_field.dart';
 import 'package:flutter_svg/svg.dart';
 
 class Body extends StatelessWidget {
+  const Body({
+    Key key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    // This size provide us total height and width of our screen
     return Background(
       child: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              "WELCOME TO HealthAI",
+              "LOGIN",
               style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: size.height * 0.05),
+            SizedBox(height: size.height * 0.03),
             SvgPicture.asset(
-              "assets/icons/chat.svg",
-              height: size.height * 0.45,
+              "assets/icons/login.svg",
+              height: size.height * 0.35,
             ),
-            SizedBox(height: size.height * 0.05),
+            SizedBox(height: size.height * 0.03),
+            RoundedInputField(
+              hintText: "Your Email",
+              onChanged: (value) {},
+            ),
+            RoundedPasswordField(
+              onChanged: (value) {},
+            ),
             RoundedButton(
               text: "LOGIN",
-              press: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return LoginScreen();
-                    },
-                  ),
-                );
-              },
+              press: () {},
             ),
-            RoundedButton(
-              text: "SIGN UP",
-              color: kPrimaryLightColor,
-              textColor: Colors.black,
+            SizedBox(height: size.height * 0.03),
+            AlreadyHaveAnAccountCheck(
               press: () {
                 Navigator.push(
                   context,
